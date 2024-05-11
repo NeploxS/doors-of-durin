31c37587802ca5c67196bbfdd55c04d9f93758ff
Michael Vetkin <mikevetkin@gmail.com>
2024-05-12 00:29:15 +0300

    Added new page

--- src/routes/reference.tsx
+++ src/routes/reference.tsx
@@ -3,7 +3,11 @@ import { AppLayout } from "@/shared/layout/ui/components/appLayout"
 export const Reference = () => {
   return (
   <AppLayout>
-    Reference
+    <div className="text-left">
+      <h2>Справка</h2>
+      <br />
+      Врата Дурина (Doors of Durin) - двери Западных Врат Кхазад-дума, или Мориа. Врата Дурина располагались на отвесных скалах, именуемых Стены Мориа, на западной стороне Туманных Гор. Они смотрели на неглубокую долину у подножия скалы.
+      </div>
   </AppLayout>
   );
 }
